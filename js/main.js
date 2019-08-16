f211b8f3ef4c530e36dbd63adc99eba0218f40e7
Welder Mileu <welderfelix21@gmail.com>
2019-08-16 17:24:39 -0300

    Em desenvolvimento

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 (function(){
     
+    const bars  = document.querySelector(".bars");
+
+    const expandir = () => {
+        bars.addEventListener("click", () => {
+            const links = document.querySelector(".menu");
+            
+            links.classList.remove("nav-remove")
+            links.classList.add("nav-add");
+            console.log("Funcioando");
+        });
+    }
+    
+    expandir()
+
+    // expandir();
 })();
